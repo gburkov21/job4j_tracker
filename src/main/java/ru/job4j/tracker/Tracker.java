089368b38c02597cac6f5a86757d2ce834746c06
g.burkov <g.burkov@cheb.eraga.net>
2022-01-12 12:27:26 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,29 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] result = new Item[size];
+        int index = 0;
+        for (Item item : items) {
+            if (item != null) {
+                result[index++] = item;
+            }
+        }
+        result = Arrays.copyOf(result, index);
+        return result;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int index = 0;
+        for (Item item : items) {
+            if (item != null && key.equals(item.getName())) {
+                result[index++] = item;
+            }
+        }
+        result = Arrays.copyOf(result, index);
+        return result;
     }
 }
