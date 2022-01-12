c63536b2086c6bb07c71ac79e29d0ebf72e22fbe
g.burkov <g.burkov@cheb.eraga.net>
2022-01-12 12:07:35 +0300

    3. Массивы и модели. [#174892 #249545]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -2,7 +2,12 @@ package ru.job4j.pojo;
 
 public class Library {
     public static void main(String[] args) {
-        Book[] bookArray = {new Book("Clean Code", 500), new Book("Head First Java", 600), new Book("Spring in Action", 700), new Book("Hibernate in Action", 800)};
+        Book firstBook = new Book("Clean Code", 500);
+        Book secondBook = new Book("Head First Java", 600);
+        Book thirdBook = new Book("Spring in Action", 700);
+        Book fourthBook = new Book("Hibernate in Action", 800);
+
+        Book[] bookArray = {firstBook, secondBook, thirdBook, fourthBook};
         for (int i = 0; i < bookArray.length; i++) {
             Book book = bookArray[i];
             System.out.println("Название: " + book.getName() + ", количество страниц: " + book.getPageNumbers());
@@ -14,7 +19,7 @@ public class Library {
             System.out.println("Название: " + book.getName() + ", количество страниц: " + book.getPageNumbers());
         }
         for (Book book : bookArray) {
-            if (book.getName().equals("Clean Code")) {
+            if ("Clean Code".equals(book.getName())) {
                 System.out.println("Название: " + book.getName() + ", количество страниц: " + book.getPageNumbers());
             }
         }
