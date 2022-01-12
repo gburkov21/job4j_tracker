9aa0d630b68828248437528ebe43aec61fdf8520
g.burkov <g.burkov@cheb.eraga.net>
2022-01-12 13:23:37 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #249887]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -49,9 +49,8 @@ public class Tracker {
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (rsl) {
-            int start = index + 1;
             int length = size - index - 1;
-            System.arraycopy(items, start, items, index, length);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
