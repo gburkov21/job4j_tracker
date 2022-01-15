8cddaf4b57fd33d24433584e792dd718b1f2286d
g.burkov <g.burkov@cheb.eraga.net>
2022-01-15 12:32:44 +0300

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,43 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public class SingleTracker {
+    private static SingleTracker singleTracker = null;
+    private Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+
+    }
+
+    private static SingleTracker getInstance() {
+        if (singleTracker == null) {
+            singleTracker = new SingleTracker();
+        }
+        return singleTracker;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+}
