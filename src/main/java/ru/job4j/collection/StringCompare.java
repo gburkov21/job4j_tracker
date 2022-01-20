df287884882e5abff2ac2ca4618ec2f8f3ce2cc7
g.burkov <g.burkov@cheb.eraga.net>
2022-01-20 23:29:34 +0300

    3. Компаратор для строк [#35008 #253482]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,12 +5,9 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        char[] firstArray;
-        char[] secondArray;
-        if (o1.length() < o2.length()) {
-            firstArray = o1.toCharArray();
-            secondArray = o2.toCharArray();
-        } else {
+        char[] firstArray = o1.toCharArray();
+        char[] secondArray = o2.toCharArray();
+        if (o2.length() < o1.length()) {
             firstArray = o2.toCharArray();
             secondArray = o1.toCharArray();
         }
