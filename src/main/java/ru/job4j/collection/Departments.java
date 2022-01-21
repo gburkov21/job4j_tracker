d7235733602736a8c5447e3c08ed23811773a9c0
g.burkov <g.burkov@cheb.eraga.net>
2022-01-21 19:13:43 +0300

    2. Отсортировать департаменты [#34352 #253839]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -3,18 +3,6 @@ package ru.job4j.collection;
 import java.util.*;
 
 public class Departments {
-    private static List<String> departments = Arrays.asList(
-            "K1",
-            "K1/SK1",
-            "K1/SK1/SSK1",
-            "K1/SK1/SSK2",
-            "K1/SK2",
-            "K2",
-            "K2/SK1",
-            "K2/SK1/SSK1",
-            "K2/SK1/SSK2"
-    );
-
     public static List<String> fillGaps(List<String> deps) {
         Set<String> tmp = new LinkedHashSet<>();
         for (String value : deps) {
