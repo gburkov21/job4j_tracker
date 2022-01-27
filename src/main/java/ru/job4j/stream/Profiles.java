a01739f5339a455f35333addcb096ded6ffc422b
g.burkov <g.burkov@cheb.eraga.net>
2022-01-27 23:38:24 +0300

    2. Список адресов [#110062 #256893]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.getAddress()).toList();
+        return profiles.stream()
+                .map(profile -> profile.getAddress())
+                .toList();
     }
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
