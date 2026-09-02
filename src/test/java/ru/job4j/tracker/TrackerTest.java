e2021c03ee7a907d35801d584db2f77e1b934b32
georgy.burkov <ds10558@mail.ru>
2026-09-02 12:41:57 +0300

    Именование классов реализующих интерфейс [#505124]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.nullValue;
 
 public class TrackerTest {
